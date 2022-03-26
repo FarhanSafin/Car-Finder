c88cb044c9e62bdbc294c975f1e59914f79e8734
Farhan <farhansafin@gmail.com>
2022-03-26 20:45:26 +0600

    Done everything

--- src/components/Buy/Buy.js
+++ src/components/Buy/Buy.js
@@ -65,6 +65,15 @@ const Buy = () => {
                     <FontAwesomeIcon icon={faTrash} className='icon' /></button>
                     <button className='button' onClick={random}>Choose one for me<FontAwesomeIcon icon={faCrosshairs} className='icon' /></button>
             </div>
+            <div className='ques-ans'>
+                <h1>Question and Answer</h1>
+                <h3>How React works?</h3>
+                <h5>React is a user interface library. It is based on the javascript programming language. In React, data flows in a single way. The JSX file format is used by React, and babel is used to transpile code into modern javascript. While working with react, we basically use 6 things. JSX, Components, Props, State, Event handler, and Data load are the things we use. In React every components passes through 3 stages. They are Mounting/Initial Stage, Update and finally Unmounting.</h5>
+                <h3>Write down difference between props and state</h3>
+                <h5>Props are data that can only be read. They can't be changed or modified. Where-else state can be modified using this.setState. For specific websites, if user changes, the state changes. </h5>
+                <h3>How useState works?</h3>
+                <h5>useState is a react hook. It is asyncronous. By calling useState we get 2 things: the state which is declared and a dispatch function. It maintains track of the state variables and makes sure they're up to current, or it re-renders them if they change.</h5>
+            </div>
         </div>
     );
 };
